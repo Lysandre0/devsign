a604df8341d582a1171645420b8567733a8ce30b
Clavier Eliott <clavier.eliott.el@gmail.com>
2022-06-27 11:16:58 +0200

    Setup routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./containers/login/login.component";
+import {DashboardComponent} from "./containers/dashboard/dashboard.component";
+import {ModuleDetailComponent} from "./containers/module-detail/module-detail.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'module/:id',
+    component: ModuleDetailComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
