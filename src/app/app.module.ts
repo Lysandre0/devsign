e12f70aadff77e524e3e512a688a95e21ea64c06
Clavier Eliott <clavier.eliott.el@gmail.com>
2022-06-27 11:12:42 +0200

    Ajout des containers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './containers/login/login.component';
+import { DashboardComponent } from './containers/dashboard/dashboard.component';
+import { ModuleDetailComponent } from './containers/module-detail/module-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    ModuleDetailComponent
   ],
   imports: [
     BrowserModule,
