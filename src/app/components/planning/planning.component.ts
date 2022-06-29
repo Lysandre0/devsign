c51d9bee465f946f77236a4e7d961eb401804770
Clavier Eliott <clavier.eliott.el@gmail.com>
2022-06-29 15:06:56 +0200

    Variabilisation planning

--- src/app/components/planning/planning.component.ts
+++ src/app/components/planning/planning.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import * as moment from 'moment';
 import { CalendarOptions } from '@fullcalendar/angular';
 import { formatDate } from '@fullcalendar/angular';
 import {Module} from "../../shared/models/module.model";
 import {modules} from "../../data/module.data";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-planning',
@@ -12,66 +13,33 @@ import {modules} from "../../data/module.data";
 })
 export class PlanningComponent implements OnInit {
 
-  public modules: Module[] = modules;
+  @Input() public modules: Module[];
 
-  // Module Id 
-  moduleId1 = modules[0].id;
-  moduleId2 = modules[1].id;
-  moduleId3 = modules[2].id;
-  moduleId4 = modules[3].id;
+  public calendarOptions: CalendarOptions = {
+    initialView: 'dayGridMonth',
+    events: [],
+    eventDisplay: 'block',
+    titleFormat: {
 
-  // Module Title
-  titleModule1 = modules[0].title;
-  titleModule2 = modules[1].title;
-  titleModule3 = modules[2].title;
-  titleModule4 = modules[3].title;
-
-  // Get Module Start Date & Change Date Format 
-  dateModule1 = modules[0].dateStart.split("/").reverse().join("/").replace(/\//g, '-');
-  dateModule2 = modules[1].dateStart.split("/").reverse().join("/").replace(/\//g, '-');
-  dateModule3 = modules[2].dateStart.split("/").reverse().join("/").replace(/\//g, '-');
-  dateModule4 = modules[3].dateStart.split("/").reverse().join("/").replace(/\//g, '-');
-  
-  Events: any[] = [];
-  
-  calendarOptions: CalendarOptions = {
-    initialView: 'dayGridMonth',  
-    dateClick: this.handleDateClick.bind(this),
-    events: [
-      { title : this.titleModule1, date: this.dateModule1, url: 'module/' + this.moduleId1 },
-      { title : this.titleModule2, date: this.dateModule2, url: 'module/' + this.moduleId2 },
-      { title : this.titleModule3, date: this.dateModule3, url: 'module/' + this.moduleId3 },
-      { title : this.titleModule4, date: this.dateModule4 , url: 'module/' + this.moduleId4 }
-    ],
-    eventClick: function(info) {
-      info.jsEvent.preventDefault(); // don't let the browser navigate
-      if (info.event.url) {
-        window.location.replace(info.event.url);
-        // window.open(info.event.url);
-      }
+    },
+    eventClick: (info) => {
+      info.jsEvent.preventDefault();
+      info.event.url && (this.router.navigate([info.event.url]));
     }
   };
-  constructor() {}
-    ngOnInit() {}
-    
-    handleDateClick(arg: any) {
-      let newDateForm = formatDate(new Date(), {
-        month: 'numeric',
-        year: 'numeric',
-        day: 'numeric'
-      });
-      console.log('Date cliked ' + arg.dateStr);
-      arg.dateStr = newDateForm;
-      // console.log(newDateForm);
-      // console.log(this.moduleId1);
-    }
 
-    
-  
-    
-   
+  constructor(
+    public router: Router
+  ) {}
 
-
-    
-
+  ngOnInit() {
+    this.calendarOptions.events = this.modules.map((mod: Module) => {
+      return {
+        title: mod.title,
+        date: moment(mod.dateStart, 'DD/MM/YYYY').format('YYYY-MM-DD'),
+        url: `module/${mod.id}`,
+        eventDisplay: 'block'
+      }
+    });
+  }
 }
